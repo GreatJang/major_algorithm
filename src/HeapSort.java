920a90805e8fa0858d3b44acc716b3799b68f6dd
GreatJang <1wkdwnsgur@gmail.com>
2023-12-28 12:42:58 +0900

    231228_commit2

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -3,46 +3,41 @@ import java.util.Arrays;
 public class HeapSort {
     public static void main(String[] args) {
         int[] arr = {7, 6, 5, 8, 3, 5, 9, 1, 6};
+        int n = arr.length;
 
 //        최초 힙구성 : n/2번만큼 heapify
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
-            heapify(arr, i);
+            heapify(arr, n, i);
         }
+        System.out.println(Arrays.toString(arr));
 
-//        최초 힙구성 이후에 최대힙과 마지막 노드와 change
-
+//        최초 힙구성 이후에 최대힙과 마지막 노드와 change해 가면서 heapify수행 // 힙정렬
+        for(int i = arr.length-1; i>=0; i--){
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+            heapify(arr, i,0);
+        }
         System.out.println(Arrays.toString(arr));
     }
 
-    static void heapify(int[] arr, int root) { //change가 발생한 부모 index root index
-//        int max_index = root;
-//        int left = root * 2 + 1;
-//        int right = root * 2 + 2;
-//        if(left<arr.length && arr[max_index] < arr[left]){
-//            max_index = left;
-//        }
-//        if(right<arr.length && arr[max_index] < arr[right]){
-//            max_index = right;
-//        }
-//        if(max_index != root){ // root보다 값이 큰index가 있다는 if문
-//            int temp = arr[root];
-//            arr[root] = arr[max_index];
-//            arr[max_index] = temp;
-//            heapify(arr, max_index);
-//        }
+    static void heapify(int[] arr, int n, int root) { // heapify
+        int max_index = root;
+        int left = root * 2 + 1;
+        int right = root * 2 + 2;
+        if(left<n && arr[max_index] < arr[left]){
+            max_index = left;
+        }
+        if(right<n && arr[max_index] < arr[right]){
+            max_index = right;
+        }
+        if(max_index != root){ // root보다 값이 큰index가 있다는 if문
+            int temp = arr[root];
+            arr[root] = arr[max_index];
+            arr[max_index] = temp;
+            heapify(arr, n, max_index);
+        }
 
-        if (arr.length > 2 * root + 1 && arr[root] < arr[2 * root + 1]) {
-            int temp = arr[root];
-            arr[root] = arr[2 * root + 1];
-            arr[2 * root + 1] = temp;
-            heapify(arr, 2 * root + 1);
-        }
-        if (arr.length > 2 * root + 2 && arr[root] < arr[2 * root + 2]) {
-            int temp = arr[root];
-            arr[root] = arr[2 * root + 2];
-            arr[2 * root + 2] = temp;
-            heapify(arr, 2 * root + 2);
-        }
 
 
 //        if (arr.length > 2 * root + 1 && arr[root] < arr[2 * root + 1] && arr[2 * root + 1]>arr[2 * root + 2]) {
